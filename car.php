2d03bbe416d122299737581af7b5fff8eca59876
Momtarin Jahan <82499244+sheiksonia@users.noreply.github.com>
2023-02-22 23:06:05 -0800

    Add files via upload

---
+++ car.php
@@ -0,0 +1,23 @@
+<!-- Write a PHP class definition for Product that includes the following:
+Private properties for name, price, and description.
+A public constructor method that accepts values for name, price, and description and sets them as the corresponding properties.
+ -->
+
+
+
+
+<?php
+class Product{
+    private $name;
+    private $price;
+    private $description;
+
+    public function __construct($name, $price, $description)
+    {
+        $this->name         = $name;
+        $this->price        = $price;
+        $this->description  = $description;
+    }
+}
+
+?>
